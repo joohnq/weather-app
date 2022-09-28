ce88bec0ad9003b24d1c1f6eb25f2332304d43bf
João Henrique <joaohenriquess3287@gmail.com>
2022-09-28 18:31:07 -0300

    feat: Adding initial structure and gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useState } from 'react'
+import { FaSearch } from "react-icons/fa";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [cityName, setCityName] = useState("")
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='app'>
+      <form>
+        <input type="text" 
+        placeholder='Digite a cidade a ser pesquisada'
+        onChange={(e) => setCityName(e.target.value)}
+        value={cityName}/>
+        <button type="submit"><FaSearch /></button>
+      </form>
     </div>
   )
 }
-
-export default App
