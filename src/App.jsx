b1fa54164ceeacba98619c10b27ca0eef1f501cf
João Henrique <joaohenriquess3287@gmail.com>
2022-09-28 19:43:16 -0300

    feat: Integration with the IBGE API and creation of option

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { FaSearch } from "react-icons/fa";
 import './App.css'
 
 export const App = () => {
-  const [cityName, setCityName] = useState("")
+  // const [countryName, setCountryName] = useState("")
+  // const [stateName, setStateName] = useState("")
+  // const [cityName, setCityName] = useState("")
+  const [ufs, setUfs] = useState([])
+  const [cities, setCities] = useState([])
+  const [selectedUf, setSelectedUf] = useState("0")
+  const [selectedCity, setSelectedCity] = useState("0")
 
+  useEffect(() => {
+    if (selectedUf === "0") {
+      return;
+    }
+    axios
+      .get(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        setCities(response.data);
+      });
+  });
+
+  useEffect(() => {
+    axios
+      .get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/")
+      .then((response) => {
+        setUfs(response.data);
+      });
+  }, [selectedUf]);
+  
   return (
     <div className='app'>
+        <h1>Weather App</h1>
       <form>
-        <input type="text" 
-        placeholder='Digite a cidade a ser pesquisada'
-        onChange={(e) => setCityName(e.target.value)}
-        value={cityName}/>
+        <select name="uf" id="uf" onChange={(e) => setSelectedUf(e.target.value)}>
+          <option value="0">Selecione a UF</option>
+          {ufs.map(uf => (
+            <option key={uf.id} value={uf.sigla}>{uf.nome}</option>
+          ))}
+        </select>
+
+        <select name="city" id="city" onChange={(e) => setSelectedCity(e.target.value)}>
+          <option value="0">Selecione a Cidade</option>
+          {cities.map(city => (
+            <option key={city.id} value={city.sigla}>{city.nome}</option>
+          ))}
+        </select>
+
         <button type="submit"><FaSearch /></button>
       </form>
     </div>
